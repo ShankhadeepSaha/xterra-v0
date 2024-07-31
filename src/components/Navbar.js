08a2be05d98f7c9f234352ea952a628a8d010165
sachin520280 <sachin520280@gmail.com>
2024-07-31 19:55:17 +0530

    features: navbar, hero and shimmer button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () => {
         <img src="https://cdn.prod.website-files.com/65a638f247db1ce7c092cd57/65b3bee77908e5d4d4a8e306_logo-web-nav-def-svg.svg" alt="Hyperlab" className="text-black h-12 w-auto" />
         {/* <span className="text-black ml-2 text-xl font-bold">Hyperlab</span> */}
       </div>
-      <div className="flex align-middle space-x-8">
-        <a href="#sports" className="text-gray-800 text-sm font-['Switzer'] hover:text-gray-900">SPORTS</a>
-        <a href="#gym" className="text-gray-800 text-sm hover:text-gray-900">GYM</a>
-        <a href="#about" className="text-gray-800 text-sm hover:text-gray-900">ABOUT US</a>
-        <a href="#contact" className="text-gray-800 text-sm hover:text-gray-900">CONTACT US</a>
+      <div className="flex align-middle space-x-16">
+        <a href="#sports" className="text-black text-sm hover:text-gray-900">SPORTS</a>
+        <a href="#gym" className="text-black text-sm hover:text-gray-900">GYM</a>
+        <a href="#about" className="text-black text-sm hover:text-gray-900">ABOUT US</a>
+        <a href="#contact" className="text-black text-sm hover:text-gray-900">CONTACT US</a>
       </div>
-      <button className="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700">
+      <button className="bg-red-600 text-white px-8 py-2 rounded-full font-bold hover:bg-red-700">
         GET HELIOS
       </button>
     </nav>
