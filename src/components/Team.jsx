9c4cc29cae785c34b421db2279efc2da048df49c
Pranshu Gupta <pranshu11111@gmail.com>
2024-09-07 18:12:34 +0530

    team images update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Founder = ({ name, title, image, description }) => (
   <div className="w-full md:w-1/2 p-4">
     <div className="bg-gray-100 rounded-3xl overflow-hidden">
-      <img src={image} alt={name} className="w-full h-80 object-cover" />
+      <img src={image} alt={name} className="w-full h-100 object-cover" />
     </div>
     <h3 className="text-3xl font-bold mt-4 mb-1 font-almirego text-[#050307]">{name}</h3>
     <p className="text-sm text-gray-600 mb-3 font-almirego">{title}</p>
@@ -16,37 +16,37 @@ function Team() {
     {
       name: "Nimesh Khandelwal",
       title: "Co-Founder & Software Lead",
-      image: "/images/Nimesh.jpg",
+      image: "/images/nimesh.svg",
       description: "IIT Kanpur Ph.D. Scholar and PMRF Fellow, specializing in Learning-Based Control. Graduated with a B.Tech in 2020 from MNIT Jaipur."
     },
     {
       name: "Avinash Bhashkar",
       title: "Co-Founder & Hardware Lead",
-      image: "/images/Avinash.jpg",
+      image: "/images/avinash.svg",
       description: "IIT Kanpur Ph.D. Scholar specializing in Hardware Design & Analysis. Gold Medalist from the M.Tech. Batch of 2021 at CAS Lucknow, with further academic experience at IISc Bangalore."
     },
     {
       name: "Amritanshu Manu",
       title: "Co-Founder & Research Lead",
-      image: "/images/Manu.jpg",
+      image: "/images/manu.svg",
       description: "IIT Kanpur Ph.D. Scholar and PMRF Fellow specializing in Planning & Control. Holder of a B.Tech-M.Tech Dual Degree from the 2021 batch at IIT Kanpur."
     },
     {
       name: "Aditya Rajawat",
       title: "Co-Founder and CEO",
-      image: "/images/Aditya.jpg",
+      image: "/images/apsr.svg",
       description: "ADAS Engineer at Jaguar Land Rover, India (2020-23), specializing in Planning & Control. Holds a B.Tech-M.Tech Dual Degree from the 2020 batch of IIT Kanpur."
     },
     {
       name: "Mangal Kothari",
       title: "Director Autonomy & Electronics",
-      image: "/images/Mangal.jpeg",
+      image: "/images/mangal.svg",
       description: "Professor in the Department of Aerospace Engineering at IIT Kanpur. Co-Founder of EndureAir Systems Pvt. Ltd. and Founder of Dronavyuh Systems Pvt. Ltd., specializing in end-to-end product development."
     },
     {
       name: "Shakti S. Gupta",
       title: "Director Hardware & Simulation",
-      image: "/images/SSG.jpg",
+      image: "/images/ssg.svg",
       description: "Professor in the Department of Mechanical Engineering at IIT Kanpur with three decades of teaching and project experience in DRDO and academia. Expert in end-to-end product development."
     }
   ];
