9c4214e1c593aa24566f31b3f7a591c989dc86aa
Pranshu Gupta <pranshu11111@gmail.com>
2024-10-09 10:20:30 +0530

    m2

--- src/components/m2/Intro.jsx
+++ src/components/m2/Intro.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useRef } from 'react'
 import Image from 'next/image';
-import Background from '/public/images/team/Img2_lab.webp';
+import Background from '/public/robot/m2.png';
 import { useScroll, useTransform, motion } from 'framer-motion';
 
 export default function Intro() {
