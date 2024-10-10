bf4bd69e14af9c23c2331dbc5fd2a8568ac9484e
Pranshu Gupta <pranshu11111@gmail.com>
2024-10-10 17:08:19 +0530

    text

--- src/app/svan-m2/page.js
+++ src/app/svan-m2/page.js
@@ -2,6 +2,10 @@
 
 import Footer from '@/components/footer/Footer'
 import Footercurve from '@/components/footer/Footercurve'
+import Description from '@/components/m2/Description';
+import HorizontalScroll from '@/components/m2/HorizontalScroll';
+import Intro from '@/components/m2/Intro';
+import Section from '@/components/m2/Section';
 import Navbar from '@/components/navbar/Navbar'
 import Lenis from "lenis";
 import React, { useEffect, useRef, useState } from "react";
@@ -20,8 +24,12 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="mt-48">
+    <div>
       <Navbar />
+      <Intro />
+      <Description />
+      <Section />
+      <HorizontalScroll />
       <Footercurve />
       <Footer />
     </div>
--- src/components/m2/Intro.jsx
+++ src/components/m2/Intro.jsx
@@ -14,10 +14,23 @@ export default function Intro() {
     const y = useTransform(scrollYProgress, [0, 1], ["0vh", "150vh"])
   
     return (
-      <div className='h-screen overflow-hidden'>
+      <div ref={container} className='h-screen overflow-hidden relative'>
         <motion.div style={{y}} className='relative h-full'>
           <Image src={Background} fill alt="image" style={{objectFit: "cover"}}/>
         </motion.div>
+        <div className='absolute inset-0 flex flex-col items-center justify-start text-center p-4 pt-16 sm:pt-20 md:pt-24'>
+          <div className='p-4 sm:p-6 md:p-8 rounded-lg max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl'>
+            <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold font-sans mb-2 sm:mb-3 text-black'>
+              SVAN M2
+            </h1>
+            <h2 className='text-xs sm:text-sm md:text-base lg:text-lg font-light mb-1 sm:mb-2 text-black'>
+              SERIES 2
+            </h2>
+            <p className='text-sm sm:text-base md:text-lg lg:text-xl font-light mt-2 sm:mt-3 md:mt-4 text-black'>
+              State-of-the-art, four-legged robotic system, meticulously engineered for exceptional mobility and agility, offering unparalleled terrain navigation capabilities in diverse environments.
+            </p>
+          </div>
+        </div>
       </div>
     )
 }
