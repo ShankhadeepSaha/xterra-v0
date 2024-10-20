a60a21cbe22844abc85943fc626ee6ea5c09bfdb
Pranshu Gupta <pranshu11111@gmail.com>
2024-10-20 14:27:57 +0530

    horizontalscroll

--- src/components/miregoWebsite/CultivateSection.js
+++ src/components/miregoWebsite/CultivateSection.js
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 function CultivateSection() {
   const images = [
-    { src: "/images/team/OutInWild_xTerra.webp", alt: "Image 1", className: "col-span-2 row-span-1 hidden sm:block" },
-    { src: "/images/team/svanM2_Lko_1.webp", alt: "Image 2", className: "col-span-2 sm:col-span-1 row-span-2 hidden sm:block" },
-    { src: "/images/team/dog_M2_2.webp", alt: "Image 3", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
-    { src: "/images/team/team.webp", alt: "Image 4", className: "col-span-2 row-span-2" },
-    { src: "/images/team/Dhaya_Lab.webp", alt: "Image 5", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
-    { src: "/images/team/avinash_lab.webp", alt: "Image 6", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
-    { src: "/images/team/teamPhoto2.webp", alt: "Image 7", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
+    { src: "/images/img/dog1.jpg", alt: "Image 1", className: "col-span-2 row-span-1 hidden sm:block" },
+    { src: "/images/img/svanM2_Lko_1.jpg", alt: "Image 2", className: "col-span-2 sm:col-span-1 row-span-2 hidden sm:block" },
+    { src: "/images/img/dog.jpg", alt: "Image 3", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
+    { src: "/images/team/team.png", alt: "Image 4", className: "col-span-2 row-span-2" },
+    { src: "/images/img/Dhaya_Lab.jpg", alt: "Image 5", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
+    { src: "/images/img/avinash_lab.jpg", alt: "Image 6", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
+    { src: "/images/img/teamPhoto2.jpg", alt: "Image 7", className: "col-span-2 sm:col-span-1 row-span-1 hidden sm:block" },
   ];
 
   return (
