0444d215e5618f570e648d4e041d264a00e99d95
Pranshu Gupta <pranshu11111@gmail.com>
2025-01-07 23:09:46 +0530

    Update Section.jsx

--- src/components/c1/Section.jsx
+++ src/components/c1/Section.jsx
@@ -4,7 +4,7 @@ const images = [
   {
     src: "/images/iitk.jpg", // Replace with your image path
     alt: "Cobot C1 Image 1",
-    className: "col-span-2 row-span-2",
+    className: "col-span-2 row-span-2", // Large section
   },
   {
     src: "/images/iitk.jpg", // Replace with your image path
